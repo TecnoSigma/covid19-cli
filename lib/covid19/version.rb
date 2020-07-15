e9730e888a048c07bc68a94c916a1761c6a1a780
Alexandre Martins <alexandre.martins@locaweb.com.br>
2020-07-15 10:30:30 -0300

    Updating gem version

--- lib/covid19/version.rb
+++ lib/covid19/version.rb
@@ -1,3 +1,3 @@
 module Covid19
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 end
--- spec/lib/covid19/version_spec.rb
+++ spec/lib/covid19/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Covid19::Client do
   it "has version latest number" do
-    expect(Covid19::VERSION).to eq('0.2.0')
+    expect(Covid19::VERSION).to eq('0.3.0')
   end
 end
